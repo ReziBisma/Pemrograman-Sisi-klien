0f4c36330a9415e0fddfc21599736dd018640c60
ReziBisma <bismarezi@gmail.com>
2026-04-09 16:34:57 +0700

    update

---
+++ app/src/App.jsx
@@ -0,0 +1,19 @@
+function Contoh(){
+    return <div>
+        <h1>Judul Berita</h1>
+    </div>
+}
+
+function Contoh2(){
+    return <div>
+        <h1>Judul Berita</h1>
+        <p>isi berita</p>
+    </div>
+}
+
+
+function App(){
+    return <h1>Halo</h1>
+}
+
+export default App;
