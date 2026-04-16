383dd1bd0936a40074a4eb31a7b68d02a99e1d63
ReziBisma <bismarezi@gmail.com>
2026-04-16 16:49:53 +0700

    pertemuan 5

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,31 +1,20 @@
-function Judul({nama}){
-    return <div>
-        <h1>Agent {nama}</h1>
-    </div>
-}
-
-function Isi(){
-    return <div>
-        <p>Septian dan Rayantol sudah sangat terpojok karena kemunculan agent bali dan agent bandung secara tiba tiba</p>
-        <p>ditengah itu tiba tiba CEO Basreng muncul, ikut merusuh. Agent CEO Basreng berubah ke wujdu silumannya</p>
-        <p>tapi nampaknya tidak akan mudah di hentikan, Septian akhrirnya menggunakan kekuatan terakhirnya yaitu memanggik</p>
-        <p>siluman dongkrak</p>
-    </div>
-}
-
-function Artikel({nama}){
-    return <div>
-        <Judul nama = {nama}/>
-        <Isi/>
-    </div>
-}
-
 function App(){
-    return  <div>
-        <Artikel nama = "Septian"/>
-        <Artikel nama = "Rayantol"/>
-        <Artikel nama = "CEO Basreng"/>
-    </div>
+
+
+    //const list_mahasiswa = [
+        //{
+           // nama: "Septian Almalik",
+            //nim: "a11.2023.1321"
+    //    },
+     //   {
+    //        nama: "Basarane Almkulik",
+            nim: "a11.2023.1233"
+    //    }
+    //]
+
+    return (<h1 className="text-3xl font-bold underline">
+        hellow World!
+    </h1>)
 }
 
 export default App;
