6210af78e32b80d5edf71f5f5adf2f83a7089ffd
ReziBisma <bismarezi@gmail.com>
2026-06-16 03:13:05 +0700

    deploy thing

--- src/Utils/AxiosInstance.jsx
+++ src/Utils/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: "http://localhost:3001", // alamat json-server
   headers: {
     "Content-Type": "application/json",
   },
