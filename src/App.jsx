347fd0cd0fe687d0c458be215e9748e71f54bc05
ReziBisma <bismarezi@gmail.com>
2026-06-16 01:30:20 +0700

    remove

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-// import Login from "@/Pages/Auth/Login/Login";
-import Mahasiswa from "./pages/Admin/Mahasiswa/Mahasiswa.jsx";
-
-const App = () => {
-    // return <Login />
-    return <Mahasiswa />
-}
-
-export default App;
